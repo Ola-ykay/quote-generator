cc0efe4efc1abebf441f95eef6176fe4a1924c06
Olayinka Fagbemi <fagbemio123@gmail.com>
2023-02-24 13:19:06 +0100

    initial-commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1 class="heading">Random Quote Generator</h1>
+    <RandomQuote />
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RandomQuote from './components/RandomQuote.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RandomQuote
   }
 }
 </script>
 
 <style>
-#app {
+.container {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10%;
+
+ 
+}
+
+.heading{
+  font-size: 2rem; 
+  font-weight: 500;
+margin-bottom: 20px;
+color: #fff;
+
+}
+*{
+  /* background: #CCCCFF; */
+  margin: 0;
+  padding: 0;
+}
+body{
+  background: #00416A;
+}
+
+@media screen and (max-width: 768px){
+  .heading{
+    font-size: 1.5rem;
+  }
+  .container{
+    margin-top: 20%;
+  }
 }
 </style>
---
+++ src/components/RandomQuote.vue
@@ -0,0 +1,159 @@
+<template>
+    <div class="quote-generator">
+<div v-if="loading" class="loading">
+<h2>Loading...</h2>
+</div>
+<div v-else class="quote-card">
+    <p>'{{ quote }}'</p>
+    <h3><bold>-</bold>{{ author }}</h3>
+</div>
+<div class="btns">
+<button @click="getQuote">Generate Quote</button>
+<button @click="copyQuote">Copy</button>
+<div v-if="showCopiedText" class="copiedText">Copied</div>
+</div>
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            loading: false,
+            quote: '',
+            author: '',
+            showCopiedText: false,
+        };
+    },
+    methods: {
+        async getQuote() {
+            this.loading = true;
+            axios.get('https://api.quotable.io/random')
+                .then(response => {
+                    this.quote = response.data.content;
+                    this.author = response.data.author;
+                    setTimeout(() => {
+                        this.loading = false;
+                    }, 2000);
+                   
+                })
+                .catch(error => {
+                    console.log(error);
+                    this.loading = false;
+                });
+           
+        },
+        copyQuote() {
+            navigator.clipboard.writeText(this.quote).then(() => {
+                this.showCopiedText = true;
+                // window.alert('copied!');
+                setTimeout(() => {
+                    this.showCopiedText = false;
+                }, 2000);
+            }).catch(err => {
+                console.log(err);
+            });
+        }
+    },
+
+    mounted() {
+        this.getQuote();
+        const link = document.createElement('link')
+    link.href ="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300&display=swap"
+    link.rel = 'stylesheet'
+    document.head.appendChild(link)
+
+    }
+};
+</script>
+
+<style scoped>
+.quote-generator {
+    width: 60%;
+    height: 35vh;
+    margin: 0 auto;
+    padding: 20px;
+    background: #fff;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+}
+.loading{
+    font-size: 1rem;
+    padding: 15px;
+    
+}
+.quote-card{
+    width: 100%;
+    font-family: 'Noto Sans', sans-serif;
+}
+.quote-card p{
+    font-size: 1rem;
+    font-weight: 500;
+    margin-bottom: 10px;
+}
+.quote-card h3{
+    font-weight: 1000;
+    font-size: 1rem;
+}
+
+.btns{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    gap: 30px;
+}
+button{
+    font-size: .9rem;
+    padding: 10px 20px;
+    border-radius: 5px;
+    background: #00416A;
+    color: #fff;
+    cursor: pointer;
+    border: none;
+    margin-top: 20px;
+}
+button:hover{
+    background: #00416A33;
+    color: #00416A;
+    font-weight: bold;
+}
+.copiedText{
+  position: fixed;
+  top: 50%;
+  left: 59%;
+  color: #00416A;
+  font-size: .9rem; 
+}
+
+@media screen and (max-width: 600px){
+    .quote-generator{
+        width: 70%;
+        height: 40vh;
+    }
+    .copiedText{
+        position: relative;
+       left: 15%;
+       top: -37%;
+    /* transform: translateX(-50%); */
+    font-size: 0.8rem;
+    }
+    .quote-card p{
+        font-size: 0.8rem;
+    }
+    .btns{
+        flex-direction: column;
+        gap: 8px;
+
+    }
+    button{
+        font-size: 0.8rem;
+        padding: 5px 10px;
+    }
+}
+</style>
